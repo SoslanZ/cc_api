1e55d71b32bf5d5ef1e7576bc9968ef0759808fe
predeinay <predeinay@gmail.com>
2017-05-16 16:29:29 +0500

    fix transaction methods #10

--- models/Ann.php
+++ models/Ann.php
@@ -48,7 +48,7 @@ class Ann extends DialPlan {
     $query = "insert into announcement( description,allow_skip,post_dest,return_ivr,noanswer,repeat_msg,recording_id)
                                 values('$description','1','ext-queues,".$__queueNum.",1','0','0','','$__recId')";
     // run transaction
-    $result = $db->begin();
+    $result = mysql_query("BEGIN");
     if (!$result) {
       //$db->closeConnection();
       throw new Exception(mysql_error());
