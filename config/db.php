27a35d3c22edaa68f55f4abd537da6f677d3900f
predeinay <predeinay@gmail.com>
2017-05-16 15:54:53 +0500

    fix transaction methods #1

--- config/db.php
+++ config/db.php
@@ -17,15 +17,15 @@ class db {
   }
 
   function begin() {
-    mysql_query("BEGIN",$this->getConnection());
+    mysql_query("START TRANSACTION;",$this->getConnection());
   }
 
   function commit() {
-    mysql_query("COMMIT",$this->getConnection());
+    mysql_query("COMMIT;",$this->getConnection());
   }
 
   function rollback() {
-    mysql_query("ROLLBACK",$this->getConnection());
+    mysql_query("ROLLBACK;",$this->getConnection());
   }
 
 }
