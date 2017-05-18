7c6044acaf558497ae15845bd549af621c6377c9
predeinay <predeinay@gmail.com>
2017-05-18 10:54:21 +0500

    add bin for queue create/delete

--- bin/queue.sh
+++ bin/queue.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-EXT_FILE_PATH=/etc/asterisk/queues_additional.conf
+QUEUE_FILE_PATH=/etc/asterisk/queues_additional.conf
+EXT_FILE_PATH=/etc/asterisk/extensions_additional.conf
 ACT=$1
 QUEUE=$2
 ARG_COUNT=0
@@ -40,15 +41,33 @@ strategy=ringall
 timeout=60
 weight=0
 wrapuptime=0
-" >> $EXT_FILE_PATH
+" >> $QUEUE_FILE_PATH
+sed -i '/include => ext-queues-custom/a \
+exten => '$QUEUE',1,Macro(user-callerid,) \
+exten => '$QUEUE',n,Answer \
+exten => '$QUEUE',n,Set(__BLKVM_OVERRIDE=BLKVM/${EXTEN}/${CHANNEL}) \
+exten => '$QUEUE',n,Set(__BLKVM_BASE=${EXTEN}) \
+exten => '$QUEUE',n,Set(DB(${BLKVM_OVERRIDE})=TRUE) \
+exten => '$QUEUE',n,ExecIf($["${REGEX("(M[(]auto-blkvm[)])" ${DIAL_OPTIONS})}" != "1"]?Set(_DIAL_OPTIONS=${DIAL_OPTIONS}M(auto-blkvm))) \
+exten => '$QUEUE',n,Set(__NODEST=${EXTEN}) \
+exten => '$QUEUE',n,Set(MONITOR_FILENAME=/var/spool/asterisk/monitor/q${EXTEN}-${STRFTIME(${EPOCH},,%Y%m%d-%H%M%S)}-${UNIQUEID}) \
+exten => '$QUEUE',n,Queue('$QUEUE',tr,,) \
+exten => '$QUEUE',n,Noop(Deleting: ${BLKVM_OVERRIDE} ${DB_DELETE(${BLKVM_OVERRIDE})}) \
+exten => '$QUEUE',n,Set(__NODEST=) \
+exten => '$QUEUE',n,Goto(app-blackhole,hangup,1) \
+exten => '$QUEUE'*,1,Macro(agent-add,'$QUEUE',) \
+exten => '$QUEUE'**,1,Macro(agent-del,'$QUEUE')' $EXT_FILE_PATH
+sed -i '/include => from-queue-custom/a \
+exten => '$QUEUE',1,Goto(from-internal,${QAGENT},1)' $EXT_FILE_PATH
 }
 
 function delete_queue {
-  sed -i '/\['$QUEUE'\]/,/^$/ { /.*/d }' $EXT_FILE_PATH
+  sed -i '/\['$QUEUE'\]/,/^$/ { /.*/d }' $QUEUE_FILE_PATH
+  sed -i '/exten => '$QUEUE'/d'
 }
 
 function add_member {
-  sed -i '/\['$QUEUE'\]/,/^$/!b;/\['$QUEUE'\]/amember=Local/'$1'@from-queue/n,0' $EXT_FILE_PATH
+  sed -i '/\['$QUEUE'\]/,/^$/!b;/\['$QUEUE'\]/amember=Local/'$1'@from-queue/n,0' $QUEUE_FILE_PATH
 }
 
 for var in "$@"
